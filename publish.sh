1efe5c38eacf3de81ec99703e7f9ad6bbbefe162
Miguel Andrade <jmandrade0@gmail.com>
2018-11-12 16:25:10 +0000

    Deployed ce4713c

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git tag -a "$1" -m "Version $1"
-npm version "$1"
-git push origin master
-git push origin --tags
-npm publish
-ember github-pages:commit --message "gh pages."
-git checkout master
