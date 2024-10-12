cc6ab42bc3640f927953a4336c2d2f4b2c62008b
Sergey Astapov <sergeastapov@gmail.com>
2024-10-11 23:06:35 -0400

    Update dependencies in docs workspace

---
+++ docs/app/instance-initializer/ember-data-for-ember-cli-addon-docs.js
@@ -0,0 +1,21 @@
+/**
+ * This is to fix an error like
+ *
+ * Could not find module `@ember-data/adapter` imported from `ember-cli-addon-docs/adapters/-addon-docs`
+ * Could not find module `@ember-data/serializer` imported from `ember-cli-addon-docs/serializers/-addon-docs`
+ *
+ * which should not happen even though ember-cli-addon-docs declares ember-data as dependency and has those imports.
+ */
+import Adapter from '@ember-data/adapter';
+import Model from '@ember-data/model';
+import Serializer from '@ember-data/serializer';
+
+export class AddonDocsAdapter extends Adapter {}
+export class AddonDocsModel extends Model {}
+export class AddonDocsSerializer extends Serializer {}
+
+function initialize() {}
+
+export default {
+  initialize,
+};
